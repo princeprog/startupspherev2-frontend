20b38fb8bc5b543c812f26b2b011ccc1088674c7
Al Prince Llavan <alprincellavan2019@gmail.com>
2025-04-26 22:09:16 +0800

    asda

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
-import "./App.css";
-import { Router, Routes, Route, Navigate } from "react-router-dom";
+import { useRef } from "react";
+import { Routes, Route } from "react-router-dom";
 import Sidebar from "./sidebar/Sidebar";
 import Startupmap from "./3dmap/Startupmap";
+
 function App() {
+  const mapInstanceRef = useRef(null);
+
   return (
     <Routes>
-      <Route path="/" element={<Sidebar />}>
-        <Route index element={<Startupmap />} />
+      <Route path="/" element={<Sidebar mapInstanceRef={mapInstanceRef} />}>
+        <Route index element={<Startupmap mapInstanceRef={mapInstanceRef} />} />
       </Route>
     </Routes>
   );
